03fc0cde6083c4978ab00b0bb86e6928aae9458a
Fabián Merchán <merchanf@javeriana.edu.co>
2015-09-05 20:03:40 -0500

    app updated to the new style changes

--- src/tgisispuj/Tgisispuj.java
+++ src/tgisispuj/Tgisispuj.java
@@ -77,18 +77,33 @@ public class Tgisispuj {
         return lss;
     }
     
+    /*
+    
+    0 = Nombre TG
+    1 = Autores
+    2 = Director
+    3 = Año
+    4 = Periodo
+    5 = Website
+    6 = Grupo de investigación
+    7 = Código
+    8 = Modalidad
+    9 = Mención de honor
+    
+    */
+    
     public static String organizeVariables( String[] str ){
         
         //Organizando Menciones de honor
-        if( str[6].equals("Si") )
-            str[6] = "<span class=\"glyphicon glyphicon-ok\"></span>";
-        else if( str[6].equals("No") )
-            str[6] = "<span class=\"glyphicon glyphicon-remove\"></span>";
+        if( str[9].equals("Si") )
+            str[9] = "<span class=\"glyphicon glyphicon-ok\"></span> "+str[9];
+        else if( str[9].equals("No") )
+            str[9] = "<span class=\"glyphicon glyphicon-remove\"></span> "+str[9];
         
         //Organizando websites
-        if( !str[9].equals("-") )
-            str[9] = "" +
-"                  <a href=\"http://"+ str[9] +"\">\n" +
+        if( !str[5].equals("-") )
+            str[5] = "" +
+"                  <a href=\"http://"+ str[5] +"\">\n" +
 "                    <button type=\"button\" class=\"btn btn-default btn-lg\">\n" +
 "                      <span class=\"glyphicon glyphicon-globe\"></span>\n" +
 "                    </button>\n" +
@@ -97,25 +112,45 @@ public class Tgisispuj {
         //General
         for( int i=0; i<str.length; i++ )
             if( str[i].equals("-") )
-                str[i] = "                  <span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span>";
+                str[i] = "<span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span>";
         
-        return ""+
-"             <tr>\n" +
-"               <td><p class=\"text-center\">"+ str[0] +"</p></td>\n" +                         //Código
-"               <td><p class=\"text-center\">"+ str[1] +"</p></td>\n" +                         //Nombre
-"               <td class=\"modality\"><p class=\"text-center\">"+ str[2] +"</p></td>\n" +      //Modalidad
-"               <td><p class=\"text-center\">"+ str[3] +"</p></td>\n" +                         //Autor
-"               <td><p class=\"text-center\">"+ str[4] +"</p></td>\n" +                         //Director
-"               <td class=\"research-group\"><p class=\"text-center\">"+ str[5] +"</p></td>\n" +                  //Grupo de investigacion
-"               <td class=\"honor-mention\"><p class=\"text-center\">"+ str[6] +"</p></td>\n" + //Mención de honor    
-"               <td class=\"year\"><p class=\"text-center\">"+ str[7] +"</p></td>\n" +          //Año
-"               <td class=\"period\"><p class=\"text-center\">"+ str[8] +"</p></td>\n" +        //Periodo
-"               <td>\n"+
-"                 <p class=\"text-center\">\n"+
-                    str[9] +"\n"+
-"                 </p>\n"+
-"               </td>\n" +                             //Website
-"             </tr>";
+        String s = ""+
+"      <div class=\"col-xs-12 item\">\n" +
+"        <div class=\"col-xs-12 main\">\n" +
+"          <div class=\"col-xs-12 col-md-5\">\n" +
+"            <p class=\"text-center\">"+str[0]+"</p>\n" + //Nombre TG
+"          </div>\n" +
+"          <div class=\"col-xs-12 col-md-3\">\n" +
+"            <p class=\"text-center\">"+str[1]+"</p>\n" + //Autores
+"          </div>\n" +
+"          <div class=\"col-xs-4 col-md-2\">\n" +
+"            <p class=\"text-center\">"+str[2]+"</p>\n" + //Director 
+"          </div>\n" +
+"          <div class=\"col-xs-4 col-md-1\">\n" +
+"            <td class=\"year\"><p class=\"text-center\">"+str[3]+"-"+str[4]+"</p>\n" + //año-periodo
+"          </div>\n" +
+"          <div class=\"col-xs-4 col-md-1\">\n" +
+"            "+str[5]+"\n" + //Website
+"          </div>\n" +
+"        </div>\n" +
+"        <div class=\"col-xs-12 secondary\">\n" +
+"          <div class=\"col-xs-4\">\n" +
+"            <p class=\"text-center\">"+str[6]+"</p>\n" + //grupo de investigación
+"          </div>\n" +
+"          <div class=\"col-xs-3\">\n" +
+"            <p class=\"text-center\">"+str[7]+"</p>\n" + //codigo
+"          </div>\n" +
+"          <div class=\"col-xs-3\">\n" +
+"            <p class=\"text-center\">"+str[8]+"</p>\n" + //Modalidad
+"          </div>\n" +
+"          <div class=\"col-xs-2\">\n" +
+"            <p class=\"text-center\">"+str[9]+"</p>\n" +
+"          </div>\n" +
+"        </div>\n" +
+"      </div>";
+        
+        //System.out.println(s);
+        return s;
     }
     
     public static void write( String fileName, List<String[]> lss ){
