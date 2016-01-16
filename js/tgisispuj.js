267076d79358224199f7f3bc14354cd98ad1e516
Fabian <merchanf@javeriana.edu.co>
2016-01-16 18:09:35 -0500

    jquery updated, footer finished and recovered some js from 1.0 version for search block

--- js/tgisispuj.js
+++ js/tgisispuj.js
@@ -7,9 +7,9 @@ var year = null;
 var general = null;
 var regex = null;
 
-$('.row').hide();
+$('.detalles-div').hide();
 $(".btn-detalles").click(function() {
-    $(this).closest('tr').next().find('.row').slideToggle("slow");
+    $(this).closest('tr').next().find('.detalles-div').slideToggle("slow");
     if($(this).text() == '+'){
         $(this).text('-');
     }else if ($(this).text() == '-') {
@@ -17,8 +17,45 @@ $(".btn-detalles").click(function() {
     }
 });
 
+function clearAttributes(){
+  $("#period-button").text("Periodo");
+  $("#modality-button").text("Modalidad");
+  $("#honor-mention-button").text("Mención de honor");
+  $("#research-group-button").text("Grupo de investigación");
+  $("#search-by-year-bar").val("");
+  $("#search-bar").val("");
+}
+
 $(".busqueda-avanzada").hide();
 //Botón de búsqueda avanzada
 $('#busqueda-avanzada-btn').click(function() {
     $(".busqueda-avanzada").slideToggle();
+});
+
+//set modality button
+$(".modality-button-item").click(function() {
+    var str = $(this).text();
+    $("#modality-button").text(str);
+});
+
+//set mencion de honor
+$(".honor-mention-button-item").click(function() {
+  var str = $(this).text();
+  $("#honor-mention-button").text(str);
+});
+
+//get period
+$(".period-button-item").click(function() {
+  var str = $(this).text();
+  $("#period-button").text(str);
+});
+
+//get research group
+$(".research-group-button-item").click(function() {
+  var str = $(this).text();
+  $("#research-group-button").text(str);
+});
+
+$("#clear-button").click(function() {
+  clearAttributes();
 });
