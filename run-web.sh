8bb694a94e3a053238503aad5f10e51fb0d93d5b
Fabian <merchanf@javeriana.edu.co>
2016-07-28 17:30:16 -0500

    se unificaron los archivos ejectuables, se añadio el tag #!/bin/sh y ademas se implemento ./wait-for-it.sh

--- run-web.sh
+++
@@ -1,8 +0,0 @@
-STATIC_URL="http://pegasus.javeriana.edu.co/static/"
-
-docker build -t tgisispuj ./Python
-docker run --name tgisispuj \
-	--link tgisispujdb:tgisispujdb \
-	-e STATIC_URL=$STATIC_URL \
-	-p 80:80 \
-	-d tgisispuj
--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 DB_ROOT_PSWD=$(openssl rand -base64 7)
 DB_NAME="tgisispujdb"
 DB_USER="tgisispujdb"
 DB_USER_PSWD=$(openssl rand -base64 7)
+STATIC_URL="http://pegasus.javeriana.edu.co/static/"
 
 docker build -t tgisispujdb ./MySQL
 docker run --name tgisispujdb \
@@ -12,3 +14,15 @@ docker run --name tgisispujdb \
     -e MYSQL_DATABASE=$DB_NAME \
     -d tgisispujdb
 
+#Tomado de https://github.com/vishnubob/wait-for-it
+./wait-for-it.sh \
+	-h 127.0.0.1 \
+	-p 3306 \
+	-t 30
+
+docker build -t tgisispuj ./Python
+docker run --name tgisispuj \
+	--link tgisispujdb:tgisispujdb \
+	-e STATIC_URL=$STATIC_URL \
+	-p 80:80 \
+	-d tgisispuj
