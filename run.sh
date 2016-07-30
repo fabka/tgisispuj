ff96d69b13f1e5948f346da03bc64d44da0a0f2d
Fabian Merchan <merchanf@javeriana.edu.co>
2016-07-30 13:34:05 -0500

    adicion de variables

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 DB_ROOT_PSWD=$(openssl rand -base64 7)
-DB_NAME="tgisispujdb"
-DB_USER="tgisispujdb"
+NOMBRE_CONTENEDOR="tgisispuj"
+NOMBRE_CONTENEDOR_BD=$NOMBRE_CONTENEDOR"db"
+NOMBRE_BD=$NOMBRE_CONTENEDOR_BD
+DB_USER="root"
 DB_USER_PSWD=$(openssl rand -base64 7)
-printenv
+DB_USER_ROOT_PSWD=$(openssl rand -base64 7)
 
-docker build -t tgisispujdb ./MySQL
-docker run --name tgisispujdb \
-	-v /var/tgisispuj:/var/lib/mysql \
-	-p 3306:3306 \
-	-e MYSQL_USER=root \
-    -e MYSQL_ROOT_PASSWORD=$DB_USER_PSWD \
-    -e MYSQL_DATABASE=$DB_NAME \
-    -d tgisispujdb
+#Contenedor MySQL
+docker build -t $NOMBRE_CONTENEDOR_BD ./MySQL
+docker run --name $NOMBRE_CONTENEDOR_BD \
+	-e MYSQL_USER=$DB_USER \
+	-e MYSQL_ROOT_PASSWORD=$DB_USER_ROOT_PSWD \
+	-e MYSQL_DATABASE=$NOMBRE_BD \
+	-d $NOMBRE_CONTENEDOR_BD
 
 #Tomado de https://github.com/vishnubob/wait-for-it
 ./wait-for-it.sh \
@@ -20,6 +21,7 @@ docker run --name tgisispujdb \
 	-p 3306 \
 	-t 30
 
+#Contenedor Django
 STATIC_URL="http://pegasus.javeriana.edu.co/static/"
 docker build -t tgisispuj ./Python
 docker run --name tgisispuj \
