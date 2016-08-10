6a8e9ea9c164e41ef2229fbb29f1f841a8484822
sala-b <merchanf@javeriana.edu.co>
2016-08-10 12:03:25 -0500

    cambios en el run para pruebas locales

--- run.sh
+++ run.sh
@@ -28,6 +28,6 @@ docker build -t tgisispuj ./Python
 docker run --name tgisispuj \
 	--link $NOMBRE_CONTENEDOR_BD:db \
 	-e STATIC_URL=$STATIC_URL \
-        -p 192.168.122.1:8084:80\
+        -p 0.0.0.0:80:80\
         -m 512M\
 	-d $NOMBRE_CONTENEDOR
